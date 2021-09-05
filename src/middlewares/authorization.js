bf965a5db9f7c8f69b4243afb4e6a4ac801480e4
Jhon Nova <jhonfredynova@gmail.com>
2021-09-05 09:45:33 -0500

    Improve syntax of api middlewares

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,30 +1,28 @@
 import firebaseAdmin from '../firebase-admin';
 
-export default (requiredClaims) => {
-  return async (req, res, next) => {
-    try {
-      let token = (req.headers['authorization'] || req.query.authorization);
+export default requiredClaims => async (req, res, next) => {
+  try {
+    let token = (req.headers['authorization'] || req.query.authorization);
 
-      // checking token
-      if (!token || !token.startsWith('Bearer ')) {
-        res.status(403).json({ code: 'auth-no-token' });
-        return;
-      }
-
-      // checking user
-      token = token.split('Bearer ')[1];
-      req.user = await firebaseAdmin.auth().verifyIdToken(token);
-      req.user.claims = Object.keys(req.user);
-
-      // checking claims
-      if (requiredClaims.length > 0 && !req.user.claims.filter(item => requiredClaims.includes(item)).length) {
-        res.status(403).json({ code: 'auth-no-privileges' });
-        return;
-      } 
-
-      next();
-    } catch (error) {
-      res.status(403).json(error);
+    // checking token
+    if (!token || !token.startsWith('Bearer ')) {
+      res.status(403).json({ code: 'auth-no-token' });
+      return;
     }
-  };
+
+    // checking user
+    token = token.split('Bearer ')[1];
+    req.user = await firebaseAdmin.auth().verifyIdToken(token);
+    req.user.claims = Object.keys(req.user);
+
+    // checking claims
+    if (requiredClaims.length > 0 && !req.user.claims.filter(item => requiredClaims.includes(item)).length) {
+      res.status(403).json({ code: 'auth-no-privileges' });
+      return;
+    } 
+
+    next();
+  } catch (error) {
+    res.status(403).json(error);
+  }
 };
--- src/middlewares/files.js
+++ src/middlewares/files.js
@@ -3,66 +3,64 @@ import os from 'os';
 import path from 'path';
 import Busboy from 'busboy';
 
-export default uploadOptions => {
-  return async (req, res, next) => {
-    try {
-      // Initialize busboy and variables
-      // Init req.body because we disable nextjs parsing
-      req.body = {};
-      let isErrorFileLimit = false;
-      const { allowedExtentions, maxFiles, maxFileSize } = uploadOptions;
-      const busboy = new Busboy({
-        headers: req.headers,
-        limits: {
-          files: maxFiles || 1,
-          fileSize: (maxFileSize || 5) * 1024 * 1024 
-        }
-      });
+export default uploadOptions => async (req, res, next) => {
+  try {
+    // Initialize busboy and variables
+    // Init req.body because we disable nextjs parsing
+    req.body = {};
+    let isErrorFileLimit = false;
+    const { allowedExtentions, maxFiles, maxFileSize } = uploadOptions;
+    const busboy = new Busboy({
+      headers: req.headers,
+      limits: {
+        files: maxFiles || 1,
+        fileSize: (maxFileSize || 5) * 1024 * 1024 
+      }
+    });
 
-      // On receive a new field
-      busboy.on('field', (fieldname, fieldValue) => {
-        try {
-          req.body[fieldname] = JSON.parse(fieldValue);
-        } catch (error) {
-          req.body[fieldname] = fieldValue;
-        }
-      });
+    // On receive a new field
+    busboy.on('field', (fieldname, fieldValue) => {
+      try {
+        req.body[fieldname] = JSON.parse(fieldValue);
+      } catch (error) {
+        req.body[fieldname] = fieldValue;
+      }
+    });
 
-      // On receive a new file
-      busboy.on('file', async (fieldname, fileData, filename) => {
-        const allowedFileExtentions = allowedExtentions || ['jpg', 'jpeg', 'png', 'gif'];
+    // On receive a new file
+    busboy.on('file', async (fieldname, fileData, filename) => {
+      const allowedFileExtentions = allowedExtentions || ['jpg', 'jpeg', 'png', 'gif'];
 
-        if (!allowedFileExtentions.find(ext => filename.endsWith('.' + ext))) {
-          return res.status(400).json({
-            code: 'uploadErrorExtentions',
-            message: allowedFileExtentions.join(', ')
-          });
-        }
-
-        fileData.on('limit', () => {
-          isErrorFileLimit = true;
-          return res.status(400).json({
-            code: 'uploadErrorSize',
-            message: maxFileSize
-          });
+      if (!allowedFileExtentions.find(ext => filename.endsWith('.' + ext))) {
+        return res.status(400).json({
+          code: 'uploadErrorExtentions',
+          message: allowedFileExtentions.join(', ')
         });
+      }
 
-        const tmpFilePath = path.join(os.tmpdir(), filename);
-        req.body[fieldname] = (req.body[fieldname] || []).concat(tmpFilePath);
-        fileData.pipe(fs.createWriteStream(tmpFilePath));
+      fileData.on('limit', () => {
+        isErrorFileLimit = true;
+        return res.status(400).json({
+          code: 'uploadErrorSize',
+          message: maxFileSize
+        });
       });
 
-      // On finish request
-      busboy.on('finish', () => {
-        if (!isErrorFileLimit) {
-          next();
-        }
-      });
+      const tmpFilePath = path.join(os.tmpdir(), filename);
+      req.body[fieldname] = (req.body[fieldname] || []).concat(tmpFilePath);
+      fileData.pipe(fs.createWriteStream(tmpFilePath));
+    });
 
-      req.pipe(busboy);
+    // On finish request
+    busboy.on('finish', () => {
+      if (!isErrorFileLimit) {
+        next();
+      }
+    });
 
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  };
+    req.pipe(busboy);
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
