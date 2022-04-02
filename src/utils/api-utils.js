7f970294cb3fe835b1ec5162ea313157a5935b4c
Jhon Nova <jhonfredynova@gmail.com>
2022-04-02 09:47:53 -0500

    Removed unneeded firebase libraries for authentication

--- src/utils/api-utils.js
+++ src/utils/api-utils.js
@@ -1,11 +1,5 @@
 export const getFirebaseAdmin = () => {
 	// eslint-disable-next-line global-require
-	require('firebase/auth');
-	// eslint-disable-next-line global-require
-	require('firebase/database');
-	// eslint-disable-next-line global-require
-	require('firebase/firestore');
-	// eslint-disable-next-line global-require
 	const firebaseAdmin = require('firebase-admin');
 
 	if (firebaseAdmin.apps.length === 0) {
