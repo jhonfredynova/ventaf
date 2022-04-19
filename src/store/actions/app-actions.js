de0908584d252ca0be45f434071f8be1531db943
Jhon Nova <jhonfredynova@gmail.com>
2022-04-18 21:33:49 -0500

    Added clean stores to app actions

--- src/store/actions/app-actions.js
+++ src/store/actions/app-actions.js
@@ -1,13 +1,17 @@
+import { TYPES as AUTH_TYPES } from './auth-actions';
 import { TYPES as CONFIG_TYPES } from './config-actions';
+import { TYPES as LOCALE_TYPES } from './locale-actions';
 import { TYPES as POST_TYPES } from './post-actions';
 import { TYPES as PROFILE_TYPES } from './profile-actions';
 
-export const TYPES = {
-	CLEAN: 'CLEANstore',
+export const APP_TYPES = {
+	CLEAN: 'CLEAN_STORE',
 };
 
 export const cleanStore = () => (dispatch) => {
+	dispatch({ type: AUTH_TYPES.CLEAN });
 	dispatch({ type: CONFIG_TYPES.CLEAN });
+	dispatch({ type: LOCALE_TYPES.CLEAN });
 	dispatch({ type: POST_TYPES.CLEAN });
 	dispatch({ type: PROFILE_TYPES.CLEAN });
 };
--- src/store/reducers/app-reducer.js
+++ src/store/reducers/app-reducer.js
@@ -1,11 +1,11 @@
-import { TYPES } from '../actions/app-actions';
+import { APP_TYPES } from '../actions/app-actions';
 
 const initialState = null;
 
 // eslint-disable-next-line default-param-last
 export default function reducer(state = initialState, action) {
 	switch (action.type) {
-		case TYPES.CLEAN:
+		case APP_TYPES.CLEAN:
 			return initialState;
 
 		default:
