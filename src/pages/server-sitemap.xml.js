dc169f2a6a408772e3ec46455702a2fb52fc598b
Jhon Nova <jhonfredynova@gmail.com>
2022-04-24 11:04:39 -0500

    Added profiles url to sitemaps

--- src/pages/server-sitemap.xml.js
+++ src/pages/server-sitemap.xml.js
@@ -3,15 +3,28 @@ import { getServerSideSitemap } from 'next-sitemap';
 import { getPostTitleSlug } from '../utils/seo-utils';
 
 export const getServerSideProps = async (ctx) => {
-	const response = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/posts/get-all`);
-	const fields = response.data.map((post) => ({
+	const postResponse = await axios.get(
+		`${process.env.NEXT_PUBLIC_SERVER_URL}/api/posts/get-all?limit=1000`
+	);
+	const profileResponse = await axios.get(
+		`${process.env.NEXT_PUBLIC_SERVER_URL}/api/profiles/get-all?limit=1000`
+	);
+
+	const postFields = postResponse.data.map((post) => ({
 		loc: `${process.env.NEXT_PUBLIC_SERVER_URL}/post/${getPostTitleSlug(post)}/${post.id}`,
 		lastmod: new Date(post.createdAt).toISOString(),
 		changefreq: 'daily',
 		priority: 0.8,
 	}));
 
-	return getServerSideSitemap(ctx, fields);
+	const profileFields = profileResponse.data.map((profile) => ({
+		loc: `${process.env.NEXT_PUBLIC_SERVER_URL}/${profile.username}`,
+		lastmod: new Date(profile.createdAt).toISOString(),
+		changefreq: 'daily',
+		priority: 0.8,
+	}));
+
+	return getServerSideSitemap(ctx, postFields.concat(profileFields));
 };
 
 export default function ServerSitemap() {}
