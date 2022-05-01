5be3ce8f0b022eb7a373208c847bfde9480cef1c
Jhon Nova <jhonfredynova@gmail.com>
2022-04-30 20:30:16 -0500

    Fixed issue with app color theme based on user preferences

--- src/components/seo.js
+++ src/components/seo.js
@@ -44,7 +44,8 @@ export default function SEO(props) {
 			<link rel="manifest" href="/site.webmanifest" />
 			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
 			<meta name="msapplication-TileColor" content="#ffc40d" />
-			<meta name="theme-color" content="#ffffff" />
+			<meta name="theme-color" content="#fff" media="(prefers-color-scheme: light)" />
+			<meta name="theme-color" content="#000" media="(prefers-color-scheme: dark)" />
 		</Head>
 	);
 }
