c1ab1cddc77b7a3710ca09c124548f83bbbfb957
Jhon Nova <jhonfredynova@gmail.com>
2022-04-30 20:31:26 -0500

    Fixed issue with preloader backhround color

--- src/components/preloader.js
+++ src/components/preloader.js
@@ -22,7 +22,7 @@ export default function Preloader() {
 					width: 13px;
 					height: 13px;
 					border-radius: 50%;
-					background: #000;
+					background: var(--color-text);
 					animation-timing-function: cubic-bezier(0, 1, 1, 0);
 				}
 				.lds-ellipsis div:nth-child(1) {
