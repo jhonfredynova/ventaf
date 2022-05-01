223fed24e1d4a163d041a0fee2815fcbc6f46599
Jhon Nova <jhonfredynova@gmail.com>
2022-05-01 16:27:07 -0500

    Added button copy clipboard compnente and implemented it on post details page

---
+++ src/components/button-copy-clipboard.js
@@ -0,0 +1,76 @@
+import React, { useRef } from 'react';
+import { copyToClipboard } from '../utils/text-utils';
+
+export default function ButtonCopyClipboard(props) {
+	const { children, className, translations, value, ...btnProps } = props;
+	const copyClipboardSucessElem = useRef();
+
+	const onClickBtnClipboard = () => {
+		copyClipboardSucessElem.current.classList.add('show');
+		copyToClipboard(value);
+		setTimeout(() => copyClipboardSucessElem.current.classList.remove('show'), 1500);
+	};
+
+	return (
+		<button
+			{...btnProps}
+			type="button"
+			className={'btn-copy-clipboard '.concat(className)}
+			onClick={onClickBtnClipboard}
+		>
+			{children}
+
+			<span ref={copyClipboardSucessElem} className="copy-clipboard-success">
+				{translations.linkWasCopied}
+			</span>
+
+			<style jsx>{`
+				.btn-copy-clipboard {
+					position: relative;
+
+					.copy-clipboard-success {
+						display: none;
+						position: absolute;
+						top: 100%;
+						right: 0;
+						background: var(--color-secondary);
+						opacity: 0.8;
+						color: var(--color-text);
+						padding: var(--spacer);
+						width: max-content;
+						z-index: 2;
+
+						&.show {
+							display: block;
+							animation: fade 1500ms ease-in-out;
+						}
+
+						@keyframes fade {
+							0% {
+								opacity: 1;
+							}
+							9% {
+								opacity: 0.9;
+							}
+							30% {
+								opacity: 0.8;
+							}
+							50% {
+								opacity: 0.6;
+							}
+							70% {
+								opacity: 0.4;
+							}
+							90% {
+								opacity: 0.2;
+							}
+							100% {
+								opacity: 0;
+							}
+						}
+					}
+				}
+			`}</style>
+		</button>
+	);
+}
--- src/components/page-post-details/breadcumb-bar.js
+++ src/components/page-post-details/breadcumb-bar.js
@@ -1,12 +1,11 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import ReactGA from 'react-ga';
 import { useRouter } from 'next/router';
-import { copyToClipboard } from '../../utils/text-utils';
 import { BREAKPOINTS } from '../../utils/style-utils';
+import ButtonCopyClipboard from '../button-copy-clipboard';
 
 export default function BreadcumbBar(props) {
 	const { translations, pageTitle, sharingUrl } = props;
-	const tooltipCopyClipboard = useRef();
 	const router = useRouter();
 
 	const onShareAd = (action, value) => {
@@ -15,12 +14,6 @@ export default function BreadcumbBar(props) {
 			action,
 			value,
 		});
-	};
-
-	const onClickBtnClipboard = () => {
-		tooltipCopyClipboard.current.classList.add('show');
-		copyToClipboard(sharingUrl);
-		setTimeout(() => tooltipCopyClipboard.current.classList.remove('show'), 1500);
 	};
 
 	return (
@@ -33,6 +26,7 @@ export default function BreadcumbBar(props) {
 			>
 				<i className="fas fa-arrow-left" /> <span className="text">{translations.backToList}</span>
 			</button>
+
 			<ul className="share-bar">
 				<li>{translations.share}:</li>
 				<li>
@@ -72,22 +66,18 @@ export default function BreadcumbBar(props) {
 					</a>
 				</li>
 				<li>
-					<button
+					<ButtonCopyClipboard
 						className="btn btn-clipboard"
-						type="button"
 						title={translations.shareViaUrl}
-						onClick={() => {
-							onClickBtnClipboard();
-							onShareAd('Copy ad url to clipboard', 2);
-						}}
+						translations={translations}
+						value={sharingUrl}
+						onClick={() => onShareAd('Copy ad url to clipboard', 2)}
 					>
 						<i className="fas fa-link fa-2x" />
-						<span ref={tooltipCopyClipboard} className="copy-clipboard-success">
-							{translations.linkWasCopied}
-						</span>
-					</button>
+					</ButtonCopyClipboard>
 				</li>
 			</ul>
+
 			<style jsx>{`
 				.breadcumb-bar {
 					display: flex;
@@ -113,52 +103,6 @@ export default function BreadcumbBar(props) {
 
 							.btn {
 								padding: 5px;
-							}
-
-							.btn-clipboard {
-								position: relative;
-
-								.copy-clipboard-success {
-									display: none;
-									position: absolute;
-									top: 100%;
-									right: 0;
-									background: var(--color-secondary);
-									opacity: 0.8;
-									color: var(--color-text);
-									padding: 3px 8px;
-									width: max-content;
-									z-index: 2;
-
-									&.show {
-										display: block;
-										animation: fade 1500ms ease-in-out;
-									}
-
-									@keyframes fade {
-										0% {
-											opacity: 1;
-										}
-										9% {
-											opacity: 0.9;
-										}
-										30% {
-											opacity: 0.8;
-										}
-										50% {
-											opacity: 0.6;
-										}
-										70% {
-											opacity: 0.4;
-										}
-										90% {
-											opacity: 0.2;
-										}
-										100% {
-											opacity: 0;
-										}
-									}
-								}
 							}
 						}
 					}
